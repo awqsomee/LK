91ce86b4098f2abef0cc004b76903a30c26b3383
Владимир <awqsome@bk.ru>
2025-03-03 19:11:51 +0300

    Reapply "release: 03 03 2025"
    This reverts commit 995187d987678c64e0e00497b5811b18163d2d08.

---
+++ build/assets/index-adff1cb8.js
@@ -0,0 +1,1 @@
+import{j as o}from"./vendor-60fdf703.js";import{N as t}from"./index-01ae0386.js";import"./index-5cf38ea0.js";import"./test-database-message-921bdcdb.js";const e=()=>o.jsx(t,{});export{e as default};
