60860dfdf018baaf139c9f9794c975afa3cb9822
Владимир <awqsome@bk.ru>
2025-05-15 15:34:35 +0300

    release: 15 05 25

---
+++ build/assets/index-4db57047.js
@@ -0,0 +1,1 @@
+import{j as o}from"./vendor-8a1e4858.js";import{N as t}from"./index-eb6e4c27.js";import"./index-60a0751e.js";import"./test-database-message-8b02f496.js";const e=()=>o.jsx(t,{});export{e as default};
---
+++ build/assets/index-c156bad0.js
@@ -0,0 +1,1 @@
+import{j as r}from"./vendor-8a1e4858.js";import{T as t}from"./index-7888a430.js";import"./consts-efab0afd.js";import"./index-60a0751e.js";import"./index-25a1de01.js";const a=()=>r.jsx(t,{isTeachers:!0});export{a as default};
