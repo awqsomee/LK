18c045421326df290dedb55e66b7da69b21dec77
Владимир <awqsome@bk.ru>
2025-06-22 13:59:17 +0300

    deploy(4005): rsv

--- src/shared/api/competence-center/api.ts
+++ src/shared/api/competence-center/api.ts
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
-import { isProduction } from '@shared/consts'
+// import { isProduction } from '@shared/consts'
 import { ApplicationsConstants } from '@shared/consts/applications'
 
 import { addAuthHeaderToRequests, getAuthResponseInterceptor } from '../config/utils'
 
-const COMPETENCE_CENTER_PROD_URL = ''
-const COMPETENCE_CENTER_DEV_URL = 'http://localhost:8800/competence-center'
+const COMPETENCE_CENTER_PROD_URL = 'https://api.mospolytech.ru/competence-center'
+// const COMPETENCE_CENTER_DEV_URL = 'http://localhost:8800/competence-center'
 
 export type CompetencePassport = {
     id: string
@@ -24,10 +24,10 @@ export type CompetenceConsultation = {
     email: string
 }
 
-export const COMPETENCE_CENTER_URL = isProduction ? COMPETENCE_CENTER_PROD_URL : COMPETENCE_CENTER_DEV_URL
-export const COMPETENCE_CENTER_WEBSOCKET_URL = 'ws://localhost:8800/competence-center/passports/status'
+// export const COMPETENCE_CENTER_URL = isProduction ? COMPETENCE_CENTER_PROD_URL : COMPETENCE_CENTER_DEV_URL
+export const COMPETENCE_CENTER_WEBSOCKET_URL = 'ws://api.mospolytech.ru/competence-center/passports/status'
 
-export const $competenceCenterApi = axios.create({ baseURL: COMPETENCE_CENTER_URL })
+export const $competenceCenterApi = axios.create({ baseURL: COMPETENCE_CENTER_PROD_URL })
 
 $competenceCenterApi.interceptors.request.use(addAuthHeaderToRequests)
 $competenceCenterApi.interceptors.response.use((response) => response, getAuthResponseInterceptor($competenceCenterApi))
