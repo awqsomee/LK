82c30935c95f264b699bdd0304668308d9e66d52
Владимир <awqsome@bk.ru>
2025-07-01 15:45:00 +0300

    release: 01 07 25

---
+++ build/assets/index-370f5aa7.js
@@ -0,0 +1,1 @@
+import{j as o}from"./vendor-8a1e4858.js";import{N as t}from"./index-da9ac38f.js";import"./index-b8d37697.js";import"./test-database-message-0887f8b3.js";const e=()=>o.jsx(t,{});export{e as default};
---
+++ build/assets/index-553e6248.js
@@ -0,0 +1,1 @@
+import{j as t}from"./vendor-8a1e4858.js";import{N as r}from"./index-da9ac38f.js";import"./index-b8d37697.js";import"./test-database-message-0887f8b3.js";const a=()=>t.jsx(r,{docsType:!0});export{a as default};
