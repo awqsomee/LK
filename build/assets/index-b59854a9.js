866982fc47764ae54f361c35b05a193f452d1ee8
Karpenko Nikita <nikkarpa@yandex.ru>
2025-07-21 23:29:03 +0300

    release: 21-07-2025

---
+++ build/assets/index-b59854a9.js
@@ -0,0 +1,1 @@
+import{j as t}from"./vendor-8a1e4858.js";import{N as r}from"./index-91ab135a.js";import"./index-5e466a61.js";import"./test-database-message-82b8f004.js";const a=()=>t.jsx(r,{docsType:!0});export{a as default};
---
+++ build/assets/index-c789809f.js
@@ -0,0 +1,1 @@
+import{j as o}from"./vendor-8a1e4858.js";import{N as t}from"./index-91ab135a.js";import"./index-5e466a61.js";import"./test-database-message-82b8f004.js";const e=()=>o.jsx(t,{});export{e as default};
