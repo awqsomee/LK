58fb2a30783dffea994bc86672078409ed6b9078
Karpenko Nikita <nikkarpa@yandex.ru>
2025-07-22 22:25:58 +0300

    release: 22 07 2025

---
+++ build/assets/index-6a1dc56d.js
@@ -0,0 +1,1 @@
+import{j as s}from"./vendor-8a1e4858.js";import{L as a}from"./links-list-76731496.js";import{k as t,P as e,az as r}from"./index-773ec141.js";const n=()=>s.jsx(t,{padding:"10px",children:s.jsx(e,{children:s.jsx(a,{blocks:r,title:"материалам",isStaff:!1})})});export{n as default};
---
+++ build/assets/index-83de1f77.js
@@ -0,0 +1,1 @@
+import{j as o}from"./vendor-8a1e4858.js";import{N as t}from"./index-121ad1d9.js";import"./index-773ec141.js";import"./test-database-message-7bc61479.js";const e=()=>o.jsx(t,{});export{e as default};
